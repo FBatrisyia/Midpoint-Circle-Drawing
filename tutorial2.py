1bfd5c51c198cac7e7227fd7879e8f89118f8300
FBatrisyia <115613270+FBatrisyia@users.noreply.github.com>
2022-11-05 18:24:16 +0800

    Update tutorial2.py

--- tutorial2.py
+++ tutorial2.py
@@ -6,22 +6,16 @@ def midpointCircleDrawing(x_center, y_center, radius):
   y = 0
   
   st.header("Midpoint Drawing Circle Algorithm")
+  st.write("x = 0, y = 0, radius = 10)
   
   #Initial point
-  print("(", x + x_center, ", ", y + y_center, ")", sep = "", end = "")
+  st.write("(", x + x_center, ", ", y + y_center, ")", sep = "", end = "")
 
   # When radius is zero only a single
   # point be printed
   if(radius > 0):
 
-    #second quad
-    st.write("(", x + x_center, ", ", -y + y_center, ")", sep = "", end = "")
-
-    #third quad
-    st.write("(", y + x_center, ",", x + y_center, ")", sep = "", end = "")
-
-    #fourth quad
-    st.write("(", -y + x_center, ",", x + y_center, ")", sep = "", end = "")
+    st.write("(", x + x_center, ", ", -y + y_center, ")", "(", y + x_center, ",", x + y_center, ")", "(", -y + x_center, ",", x + y_center, ")")
 
     #initializing p
     P = 1 - radius
@@ -45,10 +39,8 @@ def midpointCircleDrawing(x_center, y_center, radius):
                "(", x + x_center, ", ", -y + y_center,")", "(", -x + x_center, ", ", -y + y_center,")")
 
       if(x != y):
-            st.write("\n(", y + x_center, ", ", x + y_center,")", sep = "", end = "")
-            st.write("(", -y + x_center, ", ", x + y_center,")", sep = "", end = "")
-            st.write("(", y + x_center, ", ", -x + y_center, ")", sep = "", end = "")
-            st.write("(", -y + x_center, ", ", -x + y_center, ")", sep = "", end = "")
+            st.write("(", y + x_center, ", ", x + y_center,")", "(", -y + x_center, ", ", x + y_center,")", 
+                     "(", y + x_center, ", ", -x + y_center, ")", "(", -y + x_center, ", ", -x + y_center, ")")
   
 if __name__=="__main__":
     midpointCircleDrawing(0, 0, 10)
